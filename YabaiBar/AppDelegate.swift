0d0f5c9b830ae56c6d277f1e2d2b0b3127938b18
Adam Fekete <adam.fekete@kcl.ac.uk>
2019-10-19 00:45:02 +0100

    Initial Commit

---
+++ YabaiBar/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  YabaiBar
+//
+//  Created by Adam Fekete on 19/10/2019.
+//  Copyright © 2019 fekad. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ YabaiBar/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  YabaiBar
+//
+//  Created by Adam Fekete on 19/10/2019.
+//  Copyright © 2019 fekad. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
